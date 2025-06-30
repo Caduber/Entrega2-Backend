fde89ba527b3d86d33f632d0d6ca7bf86617ed75
Carlos Eduardo Drago <carlosemdl@gmail.com>
2025-06-30 02:22:58 -0300

    Mudança para handlebars e inicio da segurança

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const express = require('express');
-const path = require("path");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const hbs = require('hbs');
+const cors = require('cors');
 
 const app = express();
-app.use(express.static(path.join(__dirname, "/views")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", 'hbs');
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"/views")));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(session(
+    {
+        secret: 'segredoo',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 6000, secure: false },
+    }
+));
+
+function checkLogin(req, res, next) {
+    if (req.session.logado) {
+        next();
+    }
+    else{
+        res.redirect('login');
+    }
+}
+
 
 app.get("/", (req, res) => {
-    
-    res.send(path.join(__dirname, "/views/index.html"));
+    if (req.session.logado == true) {
+        res.render('index', {nome: req.session.nome});
+    }
+    else{
+        res.send("Você não está logado, <a href=\"/login\">Clique aqui para fazer login</a>")
+    }
 });
 
-app.get("/painel", (req, res) => {
-    res.send(path.join(__dirname, "/painel.html"))
+app.get("/login", (req, res) => {
+    req.session.logado = true;
+    req.session.nome = "TADEUUUU"
+    res.render('login');
 });
 
 app.listen(8080, () =>{
